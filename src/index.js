d34851e03a3e08bf25797945ab35f033e2d042ff
marsan9 <santos.marilyn9@gmail.com>
2023-05-04 21:19:05 +0200

    Removed console log and added relevant icon api

--- src/index.js
+++ src/index.js
@@ -31,24 +31,25 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let weatherConditionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#weather-icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   weatherConditionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.degree);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
 }
 
 let apiKey = "90e33c672cfe3413c0db44eo24acdt64";
 let unit = "imperial";
-let city = "New York City";
+let city = "Honolulu";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
 
 axios.get(apiUrl).then(displayTemperature);
